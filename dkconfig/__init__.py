d2e3005da07ada1953139d49e7c62c7c133faf9e
thebjorn <bp@datakortet.no>
2014-11-30 16:05:26 +0100

    bumpversion usage doc/specification

--- dkconfig/__init__.py
+++ dkconfig/__init__.py
@@ -1,6 +1,7 @@
 
-
-# use bumpversion --dry-run --verbose
+#
+# use: `bumpversion [--dry-run --verbose] patch` to change version numbers
+#
 __version__ = '0.1.0'
 
 from .dkconfig import parser, main
