036cb9929abdc194662abfee6702e96b9f7c0267
thebjorn <bp@datakortet.no>
2017-12-13 00:51:51 +0100

    Version has moved to canonical place.

--- tests/test_import_dkconfig.py
+++ tests/test_import_dkconfig.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import dkconfig
-import dkconfig.version
+import dkconfig._version
 import dkconfig.dkconfig
 
 
 def test_import_dkconfig():
     assert dkconfig
-    assert dkconfig.version
+    assert dkconfig._version
     assert dkconfig.dkconfig
